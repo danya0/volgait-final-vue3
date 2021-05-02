6c761f55d3b0798ee583e7ecf9fcd160ac71c0e0
danya0 <samarkin.danil@yandex.ru>
2021-05-02 10:14:35 +0300

    application logic created

--- src/store/GalleryImagesModule.js
+++ src/store/GalleryImagesModule.js
@@ -1,3 +1,27 @@
 export default {
   namespaced: true,
+  state: {
+    images: [
+      {image: 'img/images/Image1.jpg', title: 'Сирень', subtitle: 'fdsjk fdksfk fldslf lfdslf pp'},
+      {image: 'img/images/Image2.jpg', title: 'не Сирень', subtitle: 'авлал влвддвд адвдав жважвж ажв жваж'}
+    ],
+    selectedImage: 0,
+    currentImage: 0,
+  },
+  mutations: {
+    changeImage(state, newImageIndex) {
+      state.currentImage = newImageIndex
+    },
+    selectImage(state, idx) {
+      state.selectedImage = idx
+    }
+  },
+  actions: {
+    changeImage({commit}, index) {
+      commit('selectImage', index)
+      setTimeout(() => {
+        commit('changeImage', index)
+      }, 300)
+    }
+  }
 }
